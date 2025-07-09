5d56ee3e0afbb4c3a619d5cace591415c86593b2
tommyalchemy <tommy@alchemix.co>
2025-07-09 09:39:58 -0500

    Create App.tsx

---
+++ server/client/src/client/src/App.tsx
@@ -0,0 +1,38 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Home from "@/pages/home";
+import Charleston from "@/pages/charleston";
+import NewOrleans from "@/pages/new-orleans";
+import Nashville from "@/pages/nashville";
+import Layout from "@/components/layout";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/charleston" component={Charleston} />
+        <Route path="/new-orleans" component={NewOrleans} />
+        <Route path="/nashville" component={Nashville} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
